87af0a0a79b04808e33a7771e6bba192ebe879d5
xiangxw <xiangxw5689@126.com>
2012-02-25 13:55:54 +0800

    add mupdf example: export page to image file

---
+++ mupdf_example/exportpage/main.c
@@ -0,0 +1,70 @@
+/**
+ * @file main.c
+ * @brief export page
+ * @author xiangxw xiangxw5689@126.com
+ * @date 2012-02-20
+ */
+
+#include "fitz.h"
+#include "mupdf.h"
+
+int main(int argc, char **argv)
+{
+	pdf_xref *xref = NULL;
+	pdf_page *page = NULL;
+	fz_error error;
+	char *filename = NULL;
+	int index;
+
+	/* get filename */
+	if (argc != 3) {
+		printf("usage: %s filename.pdf pagenum\n", argv[0]);
+		return 1;
+	}
+	filename = argv[1];
+	index = atoi(argv[2]) - 1;
+
+	/* open xref */
+	error = pdf_open_xref(&xref, filename, NULL);
+	if (error) {
+		printf("pdf_open_xref error: %s\n", filename);
+		return 1;
+	}
+
+	/* load page tree */
+	error = pdf_load_page_tree(xref);
+	if (error) {
+		printf("pdf_load_page_tree error: %s\n", filename);
+		return 1;
+	}
+
+	/* load page */
+	error = pdf_load_page(&page, xref, index);
+	if (error) {
+		printf("can not load page %d in file %s\n", index, filename);
+		return 1;
+	}
+
+	/* render page to pixmap */
+	fz_matrix ctm = fz_translate(0, -page->mediabox.y1);
+	ctm = fz_concat(ctm, fz_scale(1.0f, -1.0f));
+	fz_bbox bbox = fz_round_rect(fz_transform_rect(ctm, page->mediabox));
+	fz_pixmap *pixmap = fz_new_pixmap_with_rect(fz_device_rgb, bbox);
+	fz_clear_pixmap_with_color(pixmap, 255);
+	fz_glyph_cache *glyphcache = fz_new_glyph_cache();
+	fz_device *device = fz_new_draw_device(glyphcache, pixmap);
+	pdf_run_page(xref, page, device, ctm);
+	fz_free_glyph_cache(glyphcache);
+	fz_free_device(device);
+
+	/* export to image file */
+	char str[100];
+	sprintf(str, "%d.png", index + 1);
+	fz_write_png(pixmap, str, 0);
+	fz_drop_pixmap(pixmap);
+
+	/* free xref and page*/
+	pdf_free_xref(xref);
+	pdf_free_page(page);
+	return 0;
+}
